89b7d0a75be5f9b7c32e037723b1b91ff54930ef
Steven Duzevich <steven@duz.ie>
2023-02-15 14:57:06 +0100

    Initial Commit

---
+++ StonksWatch Watch App/StonksWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksWatchApp.swift
+//  StonksWatch Watch App
+//
+//  Created by Steven Duzevich on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct StonksWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StonksWatch/StonksWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksWatchApp.swift
+//  StonksWatch
+//
+//  Created by Steven Duzevich on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct StonksWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
