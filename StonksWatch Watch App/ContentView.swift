ca4360d288d7daa9a6f736f1592aca6b3e560aba
Steven Duzevich <steven@duz.ie>
2023-02-22 15:18:13 +0100

    ui functionality completed

--- StonksWatch Watch App/ContentView.swift
+++ StonksWatch Watch App/ContentView.swift
@@ -12,65 +12,167 @@ import UserNotifications
 struct ContentView: View {
     @AppStorage("DATA") var data = ""
     
-    let stocksAPI = KISStocksAPI()
+    let stocksAPI: KISStocksAPI = KISStocksAPI()
     @State var quotes: [Quote] = []
-    @StateObject var portfolio = StockPortfolio()
+    @StateObject var portfolio: StockPortfolio = StockPortfolio()
+    @State var showAddModal: Bool = false
+    @State var addCode: String = ""
+    @State var addUnits: String = ""
+    @State var addAveragePurchasePrice: String = ""
+    @State var showEditModal: Bool = false
+
     
+    @Environment (\.presentationMode) var presentationMode
+    @Environment (\.dismiss) private var dismiss
     var body: some View {
-        ScrollView {
-            ForEach(portfolio.shares, id: \.self.code) { share in
+        VStack {
+            NavigationStack {
                 HStack {
-                    Text(share.code)
-                        .font(.footnote)
-                    Text("\(portfolio.getPLFromSymbol(share: share, quotes: quotes).changeDollar)")
-                        .font(.footnote)
+                    Button {
+                        Task {
+                            await refresh()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.clockwise.circle")
+                    }
+                    Button {
+                        showAddModal.toggle()
+                    } label: {
+                        Image(systemName: "plus.circle")
+                    }
                 }
-            }
-            Text("\(portfolio.getPortfolioPLTotals(quotes: quotes).totalChangeDollar)")
-            Text("\(portfolio.getPortfolioPLTotals(quotes: quotes).totalChangePC)")
-            Text("\(portfolio.getPortfolioPLTotals(quotes: quotes).totalProfitLossDollar)")
-            Text("\(portfolio.getPortfolioPLTotals(quotes: quotes).totalProfitLossPercent)")
-            
-            Button("Refresh") {
-                Task {
-                    await refresh()
+                List(portfolio.shares, id: \.self.code) { share in
+                    NavigationLink {
+                        HStack {
+                            Button {
+                                showEditModal.toggle()
+                                addCode = share.code
+                                addUnits = String(share.units)
+                                addAveragePurchasePrice = String(share.averagePurchasePrice)
+                            } label: {
+                                Image(systemName: "pencil.circle")
+                            }
+                            Button {
+                                portfolio.remove(code: share.code)
+                                data = portfolio.encode()
+                                Task {
+                                    await refresh()
+                                } // TODO: Dismiss view
+                            } label: {
+                                Image(systemName: "trash.circle")
+                            }
+                        }
+                        Text(share.code)
+                        Text("\(share.units) units")
+                        Text("Av. PP $\(share.averagePurchasePrice, specifier: "%.4f")")
+                        Text("Δ $\(portfolio.getPLFromSymbol(share: share, quotes: quotes).changeDollar, specifier: "%.2f")")
+                        Text("Δ \(portfolio.getPLFromSymbol(share: share, quotes: quotes).changePC, specifier: "%.2f")%")
+                        Text("± $\(portfolio.getPLFromSymbol(share: share, quotes: quotes).profitLossDollar, specifier: "%.2f")")
+                        Text("± \(portfolio.getPLFromSymbol(share: share, quotes: quotes).profitLossPercent, specifier: "%.2f")%")
+                    } label: {
+                        HStack {
+                            Text(share.code)
+                            Spacer()
+                            VStack {
+                                Text("Δ $\(portfolio.getPLFromSymbol(share: share, quotes: quotes).changeDollar, specifier: "%.2f")")
+                                    .font(.footnote)
+                                Text("± \(portfolio.getPLFromSymbol(share: share, quotes: quotes).profitLossPercent, specifier: "%.2f")%")
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                    .sheet(isPresented: $showEditModal) {
+                        VStack {
+                            HStack {
+                                Button {
+                                    showEditModal.toggle()
+                                    addCode = ""
+                                    addUnits = ""
+                                    addAveragePurchasePrice = ""
+                                } label: {
+                                    Image(systemName: "x.circle")
+                                }
+                                Button {
+                                    // TODO: IMPROVE ERROR CHECKING IN GENERAL
+                                    if ((Int(addUnits) != nil) && (Double(addAveragePurchasePrice) != nil)) {
+                                        portfolio.update(code: addCode, units: Int(addUnits)!, averagePurchasePrice: Double(addAveragePurchasePrice)!)
+                                        data = portfolio.encode()
+                                        Task {
+                                            await refresh()
+                                        }
+                                        showEditModal.toggle()
+                                        addCode = ""
+                                        addUnits = ""
+                                        addAveragePurchasePrice = ""
+                                    }
+                                } label: {
+                                    Image(systemName: "paperplane.circle")
+                                }
+                            }
+                            Text(addCode)
+                            TextField("\(addUnits) QUANTITY", text: $addUnits)
+                                .autocorrectionDisabled()
+                            TextField("\(addAveragePurchasePrice) AVERAGE PURCHASE PRICE", text: $addAveragePurchasePrice)
+                                .autocorrectionDisabled()
+                        }
+                        .navigationBarHidden(true)
+                    }
                 }
-            }
-            Button("QUICKADD") {
-                data = "AXE.AX/1500/1.5816|DEG.AX/6000/0.7671|ITM.AX/314/0.000|LKE.AX/2704/0.8548|NVX.AX/1000/3.8295|SCG.AX/10000/2.0282|VCX.AX/10000/1.3297"
-                Task {
-                    await refresh()
-                }
-            }
-            Button("Notify") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
+                HStack {
+                    VStack {
+                        Text("Δ $\(portfolio.getPortfolioPLTotals(quotes: quotes).totalChangeDollar, specifier: "%.2f")")
+                        Text("Δ \(portfolio.getPortfolioPLTotals(quotes: quotes).totalChangePC, specifier: "%.2f")%")
+                    }
+                    Spacer()
+                    VStack {
+                        Text("± $\(portfolio.getPortfolioPLTotals(quotes: quotes).totalProfitLossDollar, specifier: "%.2f")")
+                        Text("± \(portfolio.getPortfolioPLTotals(quotes: quotes).totalProfitLossPercent, specifier: "%.2f")%")
                     }
                 }
             }
-            Button("Notify") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-                // show this notification five seconds from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                // choose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                // add our notification request
-                UNUserNotificationCenter.current().add(request)
+            .sheet(isPresented: $showAddModal, content: {
+                VStack {
+                    HStack {
+                        Button {
+                            showAddModal.toggle()
+                            addCode = ""
+                            addUnits = ""
+                            addAveragePurchasePrice = ""
+                        } label: {
+                            Image(systemName: "x.circle")
+                        }
+                        Button {
+                            // TODO: CHECK STOCK CODE AND WAY IMPROVE ERROR CHECKING IN GENERAL
+                            if ((Int(addUnits) != nil) && (Double(addAveragePurchasePrice) != nil)) {
+                                portfolio.add(code: addCode.uppercased(), units: Int(addUnits)!, averagePurchasePrice: Double(addAveragePurchasePrice)!)
+                                data = portfolio.encode()
+                                Task {
+                                    await refresh()
+                                }
+                                showAddModal.toggle()
+                                addCode = ""
+                                addUnits = ""
+                                addAveragePurchasePrice = ""
+                            }
+                        } label: {
+                            Image(systemName: "paperplane.circle")
+                        }
+                    }
+                    TextField("CODE", text: $addCode)
+                        .autocorrectionDisabled()
+                    TextField("QUANTITY", text: $addUnits)
+                        .autocorrectionDisabled()
+                    TextField("AVERAGE PURCHASE PRICE", text: $addAveragePurchasePrice)
+                        .autocorrectionDisabled()
+                }
+                .navigationBarHidden(true)
+            })
+            .task {
+                await refresh()
             }
         }
-        .task {
-            await refresh()
-        }
     }
-
+    
     
     fileprivate func refresh() async {
         if (data != "") {
